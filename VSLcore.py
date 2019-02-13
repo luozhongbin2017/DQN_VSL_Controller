03e4e546ce14708399642f39bde482d915b541bf
ChocolateDave <ChocolateDave@users.noreply.github.com>
2019-02-13 15:09:53 +0800

    Replay optimization update
    根据训练情况修改了wrapper模式为clipped reward wrapper
    根据建议修改了记忆单元为prioritizedreplay
    修复了部分bug

--- VSLcore.py
+++ VSLcore.py
@@ -76,7 +76,8 @@ def DQNAgent():
     print("CUDA™ is " + "AVAILABLE" if torch.cuda.is_available() else "NOT AVAILABLE")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if torch.cuda.is_available():
-        args.gpu = int(input("Please assign a gpu core: "))
+        c = input("Please assign a gpu core: ")
+        args.gpu = int(c) if c is not '' else 0
         if args.gpu is not None:
             torch.cuda.set_device(args.gpu)
         print("Now using GPU CORE #{} for training".format(torch.cuda.current_device()))
@@ -95,17 +96,19 @@ def DQNAgent():
     agents = agent.DQNAgent(net, selector, writer, device = device)
 
     exp_source = experience.ExperienceSourceFirstLast(env, agents, gamma=params['gamma'], steps_count=1)
-    buffer = experience.ExperienceReplayBuffer(exp_source, params['replay_size'])
+    #buffer = experience.ExperienceReplayBuffer(exp_source, params['replay_size'])
+    buffer = experience.PrioReplayBuffer(exp_source, params['replay_size'],params['PRIO_REPLAY_ALPHA'])
     optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
 
     frame_idx = 0
-    #beta = BETA_START
+    beta = params['BETA_START']
 
     if path:
         if os.path.isfile(path):
             print("=> Loading checkpoint '{}'".format(path))
             checkpoint = torch.load(path)
             frame_idx = checkpoint['frame']
+            beta = checkpoint['Beta']
             loss_v = checkpoint['Loss']
             net.load_state_dict(checkpoint['state_dict'])
             optimizer = checkpoint['optimizer']
@@ -119,7 +122,7 @@ def DQNAgent():
             frame_idx += 1
             buffer.populate(1)
             epsilon_tracker.frame(frame_idx)
-            #beta = min(1.0, BETA_START + frame_idx * (1.0 - BETA_START) / BETA_FRAMES)
+            beta = min(1.0, params['BETA_START'] + frame_idx * (1.0 - params['BETA_START']) / params['BETA_FRAMES'])
 
             new_rewards = exp_source.pop_total_rewards()
             if new_rewards:
@@ -130,19 +133,24 @@ def DQNAgent():
                     env.close()
                     break
 
-            #Demonstrate function -> Visualization
-            '''if frame_idx % 5000 == 0:  #start evaluation
-                for i in range(3): 
-                    evaluation_agent(device, net)'''
-
             if len(buffer) < params['replay_initial']:
                 continue
 
-            optimizer.zero_grad()
+            #Regular memory optimization
+            '''optimizer.zero_grad()
             batch = buffer.sample(params['batch_size'])
             loss_v = utils.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], device=device)
             loss_v.backward()
+            optimizer.step()'''
+
+            #Prioritized memory optimization
+            optimizer.zero_grad()
+            batch, batch_indices, batch_weights = buffer.sample(params['batch_size'], beta)
+            loss_v, sample_prios_v = utils.calc_loss(batch, batch_weights, net, tgt_net.target_model,
+                                               params['gamma'], device=device)
+            loss_v.backward()
             optimizer.step()
+            buffer.update_priorities(batch_indices, sample_prios_v.data.cpu().numpy())
 
             #Writer function -> Tensorboard file
             writer.add_scalar("Loss", loss_v, frame_idx)
@@ -151,6 +159,7 @@ def DQNAgent():
             if frame_idx % 1000== 0:
                 torch.save({
                     'frame': frame_idx + 1,
+                    'Beta': beta,
                     'Loss': loss_v,
                     'state_dict': net.state_dict(),
                     'optimizer': optimizer
@@ -161,27 +170,35 @@ def DQNAgent():
                 tgt_net.sync()  #Sync q_eval and q_target
     
 
-if __name__ == '__main__':
-    DQNAgent()
-
-def demonstration_agent(device, neural_network):
-    env_evalo = Environment.SumoEnv(demonstration= True)
-    eval_selector = action.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
-    eval_epsilon_tracker = tracker.EpsilonTracker(eval_selector, params)
-    eval_agent = agent.DQNAgent(neural_network, eval_selector, device = device)
-    exp_source = experience.ExperienceSourceFirstLast(env_evalo, eval_agent, gamma=params['gamma'], steps_count=1)
+def demonstrated_agent(device):
+    dem_writer = SummaryWriter(comment = '-VSL-Dueling')
+    env_dem= Env.SumoEnv(dem_writer, demonstration= True)
+    neural_network = DuelingNetwork(env_dem.observation_space.shape, env_dem.action_space.n).to(device)
+    dem_selector = action.ArgmaxActionSelector()
+    dem_epsilon_tracker = tracker.EpsilonTracker(dem_selector, params)
+    dem_agent = agent.DQNAgent(neural_network, dem_selector, dem_writer, device = device)
+    exp_source = experience.ExperienceSourceFirstLast(env_dem, dem_agent, gamma=params['gamma'], steps_count=1)
     buffer = experience.PrioritizedReplayBuffer(exp_source, buffer_size=params['replay_size'], alpha = 0.6)
 
-    eval_idx = 0
+    dem_idx = 0
 
-    with tracker.RewardTracker(eval_writer, params['stop_reward'], params['stop_frame']) as reward_tracker:
+    with tracker.RewardTracker(dem_writer, params['stop_reward'], params['stop_frame']) as reward_tracker:
         while True:
-            eval_idx += 1
+            dem_idx += 1
             buffer.populate(1)
-            eval_epsilon_tracker.frame(eval_idx)
+            dem_epsilon_tracker.frame(dem_idx)
 
             new_rewards = exp_source.pop_total_rewards()
             if new_rewards:
-                if reward_tracker.reward(new_rewards[0], eval_idx, eval_selector.epsilon):
+                if reward_tracker.reward(new_rewards[0], dem_idx):
                     break
+
+if __name__ == '__main__':
+    #dem = input('Demonstrated? y/n ')
+    dem = 'n'
+    #Demonstrate function -> Visualization
+    if dem == 'y' or 'Y':
+        demonstrated_agent("cuda")
+    else:
+        DQNAgent()
 
--- common/experience.py
+++ common/experience.py
@@ -463,6 +463,48 @@ class PrioritizedReplayBuffer(ExperienceReplayBuffer):
 
             self._max_priority = max(self._max_priority, priority)
 
+class PrioReplayBuffer:
+    def __init__(self, exp_source, buf_size, prob_alpha=0.6):
+        self.exp_source_iter = iter(exp_source)
+        self.prob_alpha = prob_alpha
+        self.capacity = buf_size
+        self.pos = 0
+        self.buffer = []
+        self.priorities = np.zeros((buf_size, ), dtype=np.float32)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def populate(self, count):
+        max_prio = self.priorities.max() if self.buffer else 1.0
+        for _ in range(count):
+            sample = next(self.exp_source_iter)
+            if len(self.buffer) < self.capacity:
+                self.buffer.append(sample)
+            else:
+                self.buffer[self.pos] = sample
+            self.priorities[self.pos] = max_prio
+            self.pos = (self.pos + 1) % self.capacity
+
+    def sample(self, batch_size, beta=0.4):
+        if len(self.buffer) == self.capacity:
+            prios = self.priorities
+        else:
+            prios = self.priorities[:self.pos]
+        probs = prios ** self.prob_alpha
+
+        probs /= probs.sum()
+        indices = np.random.choice(len(self.buffer), batch_size, p=probs)
+        samples = [self.buffer[idx] for idx in indices]
+        total = len(self.buffer)
+        weights = (total * probs[indices]) ** (-beta)
+        weights /= weights.max()
+        return samples, indices, np.array(weights, dtype=np.float32)
+
+    def update_priorities(self, batch_indices, batch_priorities):
+        for idx, prio in zip(batch_indices, batch_priorities):
+            self.priorities[idx] = prio
+
 
 class BatchPreprocessor:
     """
